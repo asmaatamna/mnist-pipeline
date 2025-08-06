62b9d862c6777ea2f1deac1bf4394d793836fd37
asmaatamna <atamnaasmaines@gmail.com>
2025-08-06 21:49:04 +0200

    Create TestClient in a with statement to trigger app startup

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,9 +4,6 @@ from PIL import Image
 import io
 
 
-client = TestClient(app)
-
-
 def test_predict_endpoint():
     # Create a dummy 28x28 grayscale image
     image = Image.new("L", (28, 28), color=255)
@@ -14,8 +11,10 @@ def test_predict_endpoint():
     image.save(img_bytes, format='PNG')
     img_bytes.seek(0)
 
-    # Send as multipart/form-data
-    response = client.post("/predict", files={"file": ("test.png", img_bytes, "image/png")})
+    # Use TestClient as a context manager to trigger startup events
+    with TestClient(app) as client:
+        # Send input as multipart/form-data
+        response = client.post("/predict", files={"file": ("test.png", img_bytes, "image/png")})
 
     assert response.status_code == 200
     assert "prediction" in response.json()
